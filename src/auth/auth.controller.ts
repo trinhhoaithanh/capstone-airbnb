cc5f91e3958318acf3b12224c7d429d337dcd9b7
Trinh Hoai Thanh <91533983+trinhhoaithanh@users.noreply.github.com>
2023-07-26 22:18:02 +0700

    sign up swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,6 +3,34 @@ import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { PrismaClient } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger/dist';
+class userType{
+
+  
+  @ApiProperty()
+  email:String;
+
+  @ApiProperty()
+  pass_word:String;
+
+  @ApiProperty()
+  full_name:String;
+
+  @ApiProperty()
+  birth_day:String;
+
+  @ApiProperty()
+  gender:Boolean;
+
+  @ApiProperty()
+  user_role:String;
+
+  @ApiProperty()
+  phone:String;
+
+
+
+}
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -10,8 +38,9 @@ export class AuthController {
   prisma = new PrismaClient();
   
   // Sign up 
+  @HttpCode(201)
   @Post("/sign-up")
-  signUp(@Body() body){
+  signUp(@Body() body:userType){
     return this.authService.signUp(body);
   } 
   
