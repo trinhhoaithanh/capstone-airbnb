d4ec7d0ccf33c075998e2faf8c89641c65e2f858
Ngan LE <ngan.ltkn2911@gmail.com>
2023-08-07 17:47:54 +0200

    created location

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -1,10 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { LocationService } from './location.service';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
+import { Location } from './entities/location.entity';
+import { ApiHeader } from '@nestjs/swagger';
 
 @Controller('location')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
 
+  // Create location
+  @ApiHeader({
+    name: "token",
+    description: "Your authentication token",
+    required: true
+  })
+  @Post("create-location")
+  createLocation(@Headers("token") token, @Body() location: Location) {
+    return this.locationService.createLocation(token, location); 
+  }
 }
