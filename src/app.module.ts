05e567174d7d6bfaa3a4d8a1ffdaedeab4479829
Ngan LE <ngan.ltkn2911@gmail.com>
2023-08-07 14:19:39 +0200

    created model reservations

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,9 @@ import { UsersModule } from './users/users.module';
 import { ReviewsModule } from './reviews/reviews.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { LocationModule } from './location/location.module';
+import { ReservationsModule } from './reservations/reservations.module';
 @Module({
-  imports: [AuthModule,ConfigModule.forRoot({isGlobal: true}), UsersModule, ReviewsModule, RoomsModule, LocationModule],
+  imports: [AuthModule,ConfigModule.forRoot({isGlobal: true}), UsersModule, ReviewsModule, RoomsModule, LocationModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/reservations/reservations.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReservationDto } from './dto/create-reservation.dto';
+import { UpdateReservationDto } from './dto/update-reservation.dto';
+
+@Injectable()
+export class ReservationsService {
+  
+}
