fdbf3836b986c27c05485a64b87e9278bd2797d1
Ngan LE <ngan.ltkn2911@gmail.com>
2023-08-08 11:05:23 +0200

    added apitags for reservations

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -2,7 +2,9 @@ import { Controller, Get} from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { CreateReservationDto } from './dto/create-reservation.dto';
 import { UpdateReservationDto } from './dto/update-reservation.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Reservations")
 @Controller('reservations')
 export class ReservationsController {
   constructor(private readonly reservationsService: ReservationsService) {}
